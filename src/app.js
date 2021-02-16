560a2abe3afc171443586bb88c5f33276cb17537
Rogelio Delgado (@RogFed) <rogelio@rogfed.ninja>
2021-02-16 01:31:16 -0600

    Add Components directory

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const App = () => (
-  <>
-    <h1>ReactJS Hello World!</h1>
-  </>
-)
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
