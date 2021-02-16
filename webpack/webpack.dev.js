cfd56b664af7fbc96afd933da7b00ac190664aa5
Rogelio Delgado (@RogFed) <rogelio@rogfed.ninja>
2021-02-15 19:26:05 -0600

    Add Basic Webpack Config

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,19 @@
+const {merge} = require('webpack-merge')
+const common = require('../webpack.common')
+const path = require('path');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    compress: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    hot: true,
+    liveReload: false,
+    open: true,
+    port: 8080,
+    useLocalIp: true,
+  }
+})
