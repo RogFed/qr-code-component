1d25a120fe224375d38a4a63a87944aa955897c4
Rogelio Delgado (@RogFed) <rogelio@rogfed.ninja>
2021-09-06 17:18:44 -0500

    Add support for image assets

--- webpack.common.js
+++ webpack.common.js
@@ -24,6 +24,10 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
