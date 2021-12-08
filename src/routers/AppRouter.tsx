8d6ec1917f4aa06bfee1b10aaeec3a3c1a87f4d0
Rogelio Delgado <rogelio@rogfed.ninja>
2021-12-08 00:46:24 +0000

    Update dependencies

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import App from '../components/App'
-import NotFound from '../components/NotFound'
+import App from '../components/App';
 
 const AppRouter = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path='/' component={App} />
-      <Route component={NotFound} />
-    </Switch>
+    <Routes>
+      <Route path='/' element={<App />} />
+    </Routes>
   </BrowserRouter>
-)
+);
 
-export default AppRouter
+export default AppRouter;
